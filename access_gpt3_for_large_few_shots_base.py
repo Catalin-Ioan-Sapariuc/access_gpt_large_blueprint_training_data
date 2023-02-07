eb90964ca6ae48db7aad1487e4815651e5d95546
catalin <ioan.sapariuc@cultofcoders.com>
2023-02-07 11:03:49 +0200

    added tokenization, optimized GPT3 answer by adding a stopping sequence

--- access_gpt3_for_large_few_shots_base.py
+++ access_gpt3_for_large_few_shots_base.py
@@ -14,15 +14,15 @@ import openai
 import pandas as pd
 import numpy as np
 
-embedding_engine = "text-similarity-davinci-001"
-#embedding_engine = "text-embedding-ada-002" 
+#embedding_engine = "text-similarity-davinci-001"
+embedding_engine = "text-embedding-ada-002" 
 engine='text-davinci-003'
 temperature=0.
 top_p=1.
 presence_penalty=0.
-max_tokens=300
+max_tokens=600
 
-def text_embed(text):
+def text_embed(text:str) -> str:
     response = openai.Embedding.create(input=text,engine=embedding_engine)
     curated_response = response.data[0]["embedding"]
     return curated_response
@@ -30,44 +30,47 @@ def text_embed(text):
 def cosine_similarity(A,B):
     return np.dot(A,B)/(np.linalg.norm(A)*np.linalg.norm(B))
 
-df = pd.read_pickle('blueprint-primer-general-and-specific-embeddings.pkl')
+df = pd.read_pickle('blueprint-primer-general-and-specific-embeddings-with-tokens.pkl')
 
-print(df.head(3))
-print(type(df['prompt_embedding'][0]))
-query = "What is blueprint?"
-#query = '''Using the blueprint tools, write steps to create an e-shopping ai, 
-#with collections of users,
-#products, shopping events and users balance, and with relations between these.'''
+#print(df.head(3))
+#print(type(df['prompt_embedding'][0]))
+
+#query = "What is blueprint?"
+query = '''Using the blueprint tools, write steps to create an e-shopping ai, 
+with collections of users,
+products, shopping events and users balance, and with relations between these.'''
 
 query_embed = text_embed(query)
 
 df['query_similarity']=df.apply(lambda x: cosine_similarity(x['prompt_embedding'], query_embed), axis=1)
+#print('similarity between query and the first prompt in the knowledge database is: ', df['query_similarity'][0])
 
-print('the df head after adding the query similarity \n')
-print(df.head(3))
+
+#print('the df head after adding the query similarity \n')
+#print(df.head(3))
 
 dfs = df.sort_values(by='query_similarity',ascending=False)
 
-print('the df head after sorting by query similarity \n')
-print(dfs.head(4))
+#print('the df head after sorting by query similarity \n')
+#print(dfs.head(4))
 
 n=5; ## how many similar prompts to use (from the knowledge database)
 actual_prompt=''
 for i in range(n):
-    actual_prompt += 'input: '+ dfs['prompt'][i]+ ' output: ' + dfs['completion'][i]+' '+ ' \n'
-actual_prompt += 'input: ' + query
+    actual_prompt += 'input: '+ dfs['prompt'][i]+ ' output: ' + dfs['completion'][i]+' '+ '\n'
+actual_prompt += 'input: ' + query + ' output: '
 
-print(len(actual_prompt))
+#print(len(actual_prompt))
 if (len(actual_prompt) > 10000):
     print('warning: you are reaching the max allowed number of tokens since len(actual_prompt)= ', len(actual_prompt))
 
-print('The first 200 characters of the actual prompt are: \n')
-print(actual_prompt[:200])
-print('The last 100 characters of the actual prompt are: \n')
-print(actual_prompt[-100:])
+#print('The first 200 characters of the actual prompt are: \n')
+#print(actual_prompt[:200])
+#print('The last 100 characters of the actual prompt are: \n')
+#print(actual_prompt[-100:])
 
 response = openai.Completion.create(engine=engine, prompt= actual_prompt, max_tokens= max_tokens,
-           temperature=temperature, top_p=top_p, presence_penalty=presence_penalty, n=1)
+           temperature=temperature, top_p=top_p, presence_penalty=presence_penalty, n=1, stop="\n\ninput:")
 
 
 print('The GPT 3 engine responds with: ', response['choices'][0]['text']) 
---
+++ embed_examples_with_tokens.py
@@ -0,0 +1,47 @@
+## read (prompt, completion) pairs from csv file . 
+## embed the prompt using the text-similarity-davinci-001 engine embedding (openai)
+## then saved the full embeedded df in a new .csv file (blueprint-primer-general-and-specific-embeddings.csv)
+
+import pickle
+import openai
+import tiktoken
+import pandas as pd
+import numpy as np
+
+#embedding_engine = "text-similarity-davinci-001"
+embedding_engine = "text-embedding-ada-002" 
+# possible engines (encoding_names) to consider for num_tokens_from_string
+# are: gpt2 (for all gpt3 embeddings), "p50k_base" (for text-davinci-002 and text-davinci-003 and
+# "cl100k_base" for text-embedding-ada-002 
+
+def text_embed(text: str) -> str:
+    response = openai.Embedding.create(input=text,engine=embedding_engine)
+    curated_response = response.data[0]["embedding"]
+    return curated_response
+
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+df = pd.read_excel('blueprint-primer-general-and-specific.xlsx')
+
+print('the size of original df from blueprint-primer-general-and-specific.csv \n')
+print(df.shape)
+
+#df.replace(r'\s+|\\n', ' ', regex=True, inplace=True)
+#df.replace(r'\s+|\"', '', regex=True, inplace=True)
+
+#print('the df head after replacing all the newlines with spaces \n')
+#print(df.head(3))
+
+df['prompt_embedding']=df['prompt'].map(text_embed)
+
+df['prompt_and_completion_tokens']=df.apply(lambda x: num_tokens_from_string(x['prompt'], "p50k_base" ) + num_tokens_from_string(x['completion'],"p50k_base"), axis=1)
+
+#print(df.head(5))
+
+df.to_pickle('blueprint-primer-general-and-specific-embeddings-with-tokens.pkl')
+
